0f675d0650019970c543968c2186b1d64a51e5ca
RWEC\akrecev <akretsev@gmail.com>
2023-09-21 13:40:37 +0300

    properties references

--- src/main/java/com/kretsev/study/MusicPlayer.java
+++ src/main/java/com/kretsev/study/MusicPlayer.java
@@ -1,14 +1,50 @@
 package com.kretsev.study;
 
 public class MusicPlayer {
+    private String name;
+    private Integer volume;
     private Music music;
+
+    public MusicPlayer() {
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer(Integer volume, Music music) {
+        this.volume = volume;
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic() {
+        System.out.println("Player name: " + getName());
         System.out.println("Playing song: " + music.getSong());
+        System.out.println("Volume level: " + getVolume());
     }
 
 }
